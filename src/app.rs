ce87e37b305693ce191f9fcdebdb2783c276e905
juaniwk3 <juaniwk3@gmail.com>
2023-02-15 23:50:12 -0300

    feat: scss, get quote on useEffect, update on input

--- src/app.rs
+++ src/app.rs
@@ -1,49 +1,72 @@
-use serde::{Deserialize, Serialize};
-use serde_wasm_bindgen::to_value;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::spawn_local;
+use web_sys::HtmlInputElement;
 use yew::prelude::*;
 
 #[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "tauri"])]
-    async fn invoke(cmd: &str, args: JsValue) -> JsValue;
+    // async fn invoke(cmd: &str, args: JsValue) -> JsValue;
+    async fn invoke(cmd: &str) -> JsValue;
 
     #[wasm_bindgen(js_namespace = console)]
     fn log(s: &str);
-}
-
-#[derive(Serialize, Deserialize)]
-struct GreetArgs<'a> {
-    name: &'a str,
 }
 
 #[function_component(App)]
 pub fn app() -> Html {
     let input_ref = use_node_ref();
 
-    let quote = String::from("The quick brown fox jumps over the lazy dog");
+    let quote = use_state(|| "Loading quote...".to_string());
+
+    let user_quote = use_state(|| "".to_string());
+
+    let oninput = Callback::from(move |input_event: InputEvent| {
+        let target: HtmlInputElement = input_event
+            .target()
+            .unwrap_throw()
+            .dyn_into()
+            .unwrap_throw();
+
+        let input_value = target.value().to_string();
+
+        user_quote.set(input_value.clone());
+        log(&input_value);
+    });
+
+    {
+        let quote = quote.clone();
+
+        use_effect_with_deps(
+            move |_| {
+                spawn_local(async move {
+                    quote.set(invoke("start").await.as_string().unwrap());
+                });
+                || ()
+            },
+            (),
+        );
+    }
 
     html! {
         <main class="container">
-            <h1 className="title">
+            <h1 class="title">
                 {"Welcome to "}<i>{"Typas"}</i>{"!"}
             </h1>
 
             <div class="progress" />
 
             <code class="quote">
-                <p>{quote}</p>
+                <p>{(*quote).clone()}</p>
             </code>
 
             <input
                 id="greet-input"
-                //   onChange={(e) => handleType(e)}
                 placeholder="Type here..."
-                //   maxLength={quote.length}
                 autoComplete="false"
                 ref={input_ref}
                 type="text"
+                {oninput}
             />
 
             <div className="row">
